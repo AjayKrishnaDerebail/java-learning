b804f2d6488cb3bb56aa5c8116cba3f1e13241ff
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2024-05-05 16:45:02 +0530

    Updated hibernate configurations

--- java/database/hibernate/src/main/java/com/hibernate/Main.java
+++ java/database/hibernate/src/main/java/com/hibernate/Main.java
@@ -5,14 +5,16 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-import java.io.IOException;
 import java.util.Date;
 
 public class Main {
-    public static void main (String[] args) throws IOException {
+    public static void main (String[] args) {
 
         SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class).buildSessionFactory();
         Student student = new Student(103, "Kane", "Washington DC");
+        Student student1 = new Student(104, "Steve Austin", "Texas");
+        Student student2 = new Student(105, "Undertaker", "Death Valley");
+        Student student3 = new Student(106, "Khali", "Punjab");
         Address address = new Address();
         address.setStreet("Chinmaya colony , Hosabettu");
         address.setCity("Surathkal,Mangaluru");
@@ -24,11 +26,13 @@ public class Main {
         try (Session session = sessionFactory.openSession()) {
             Transaction tx;
             tx = session.beginTransaction();
-            //session.persist(student);
+            session.persist(student);
+            session.persist(student1);
+            session.persist(student2);
+            session.persist(student3);
             session.persist(address);
             tx.commit(); // same as session.getTransaction().commit();
         }
-
 
 
     }
