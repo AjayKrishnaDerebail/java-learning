ea78a445bd2075e4bbc52d9c08b6f09a1b624578
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2024-05-11 00:09:42 +0530

    Defining CRUD operations

--- spring/spring-orm/src/main/java/com/spring/orm/repositories/StudentDao.java
+++ spring/spring-orm/src/main/java/com/spring/orm/repositories/StudentDao.java
@@ -2,7 +2,18 @@ package com.spring.orm.repositories;
 
 import com.spring.orm.entities.Student;
 
+import java.util.List;
+
 public interface StudentDao {
+
     public int insert (Student student);
+
+    public void deleteStudent (int studentId);
+
+    public Student getStudent (int studentId);
+
+    public List<Student> getAllStudents ();
+
+    public void updateStudent (Student student);
 
 }
