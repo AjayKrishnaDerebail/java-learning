0d8cb62e64acfff6ab7e51aa90da3354199cbed8
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2024-05-11 00:09:42 +0530

    Defining CRUD operations

--- spring/spring-orm/src/main/java/com/spring/orm/repositories/StudentDaoImpl.java
+++ spring/spring-orm/src/main/java/com/spring/orm/repositories/StudentDaoImpl.java
@@ -5,6 +5,7 @@ import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Component
 public class StudentDaoImpl implements StudentDao {
@@ -17,6 +18,32 @@ public class StudentDaoImpl implements StudentDao {
         numberOfRecords = (Integer) this.hibernateTemplate.save(student);
         return numberOfRecords;
     }
+
+    @Override
+    public Student getStudent (int studentId) {
+        return this.hibernateTemplate.get(Student.class, studentId);
+    }
+
+    @Override
+    public List<Student> getAllStudents () {
+        return this.hibernateTemplate.loadAll(Student.class);
+    }
+
+    @Override
+    @Transactional
+    public void deleteStudent (int studentId) {
+        Student student = this.hibernateTemplate.get(Student.class, studentId);
+        if(student != null) {
+            this.hibernateTemplate.delete(student);
+        }
+    }
+
+    @Override
+    @Transactional
+    public void updateStudent (Student student) {
+        this.hibernateTemplate.update(student);
+    }
+
 
     @SuppressWarnings("unused")
     public HibernateTemplate getHibernateTemplate () {
