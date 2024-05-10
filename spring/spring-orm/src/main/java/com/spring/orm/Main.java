1ccd88a0ae3b162c1716955a3a1ce3b3c7c58daf
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2024-05-10 20:41:59 +0530

    Spring ORM initial code

--- spring/spring-orm/src/main/java/com/spring/orm/Main.java
+++ spring/spring-orm/src/main/java/com/spring/orm/Main.java
@@ -1,7 +1,20 @@
 package com.spring.orm;
 
+import com.spring.orm.entities.Student;
+import com.spring.orm.repositories.StudentDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Hello world!");
+        ApplicationContext context = new ClassPathXmlApplicationContext("ormconfig.xml");
+        StudentDao studentDao = context.getBean("studentDao", StudentDao.class);
+        Student student = new Student(1,"Eren","Shiganshina");
+        try{
+            int result = studentDao.insert(student);
+            System.out.println(result);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
