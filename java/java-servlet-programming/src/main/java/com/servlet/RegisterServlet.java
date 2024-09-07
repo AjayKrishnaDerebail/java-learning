bc3bcce07004e78f362caf072abca32754631178
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2024-09-08 00:24:12 +0530

    Demonstration of RequestDispatcher methods and welcome-file-list

--- java/java-servlet-programming/src/main/java/com/servlet/RegisterServlet.java
+++ java/java-servlet-programming/src/main/java/com/servlet/RegisterServlet.java
@@ -1,5 +1,7 @@
 package com.servlet;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,7 +11,7 @@ import java.io.PrintWriter;
 import java.util.Objects;
 
 public class RegisterServlet extends HttpServlet {
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         System.out.println("Welcome to register Servlet");
         response.setContentType("text/html");
 
@@ -30,9 +32,15 @@ public class RegisterServlet extends HttpServlet {
                 out.println("<div>Gender is " + gender + "</div>");
                 out.println("<div>Course is " + course + "</div>");
             }
+            // Request is forwarded to another endpoint itself the output above of form contents won't be displayed
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("success");
+            requestDispatcher.forward(request,response);
         }
         else {
             out.println("<div>Please accept terms and condition</div>");
+            //Demonstration of RequestDispatcher include() method
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("form.html");
+            requestDispatcher.include(request, response);
         }
         out.println("<div><a href=\"form.html\">Click here to go back to form main screen</a> </div>");
     }
