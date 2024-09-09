72aacaed059cd13106de9166e9a437fcc633a45d
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2024-09-09 23:58:48 +0530

    Code refactoring

---
+++ java/java-servlet-programming/src/main/java/com/servlet/basics/HttpServletProg.java
@@ -0,0 +1,18 @@
+package com.servlet.basics;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HttpServletProg extends HttpServlet {
+    public void doGet(HttpServletRequest request , HttpServletResponse response) throws IOException {
+        System.out.println("This is get method");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>This is from doGet method </h1>");
+        out.println("<div> <a href=\"index.html\">Click here to go back to index</a> </div>");
+    }
+}
