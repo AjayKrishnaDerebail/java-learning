37195c4dc5de81c548ab07e68c891f473754d963
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2025-01-13 23:06:42 +0530

    Learning model to post data from backend to frontend

--- spring/spring-mvc/src/main/java/controller/HomeController.java
+++ spring/spring-mvc/src/main/java/controller/HomeController.java
@@ -2,16 +2,17 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String home(){
+    public String home(Model model){
         System.out.println("Inside home method of controller");
         System.out.println("This returns default view of index.jsp");
+        model.addAttribute("message", "Welcome to Spring MVC");
+        
         return "index";
     }
 
--- spring/spring-mvc/src/main/webapp/templates/index.jsp
+++ spring/spring-mvc/src/main/webapp/templates/index.jsp
@@ -1,5 +1,9 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<%
+    String message = (String) request.getAttribute("message");
+%>
+    <h1>Message is <%= message %></h1>
 </body>
 </html>
