2774b56d7e9cf7e77db00989b21355027a14801d
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2025-03-02 23:06:11 +0530

    File uploading using spring mvc

--- spring/spring-mvc/src/main/java/com/spring/mvc/controller/FileUploadController.java
+++ spring/spring-mvc/src/main/java/com/spring/mvc/controller/FileUploadController.java
@@ -1,7 +1,10 @@
 package com.spring.mvc.controller;
 
+import jakarta.servlet.http.HttpSession;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,12 +19,27 @@ public class FileUploadController {
   }
 
   @RequestMapping(path = "/uploadImage" ,method = RequestMethod.POST)
-  public String fileUpload(@RequestParam("profile") MultipartFile file) throws IOException {
+  public String fileUpload(@RequestParam("profile") MultipartFile file, HttpSession session , Model model) throws IOException {
     System.out.println(file.getSize());
     System.out.println(file.getOriginalFilename());
     System.out.println(file.getContentType());
     System.out.println(file.getName());
     System.out.println(file.getInputStream());
+
+    byte[] data = file.getBytes();
+    String path =
+        session.getServletContext().getRealPath("\\") + "\\" + "static" + "\\" + "images"
+            + "\\"
+        + file.getOriginalFilename();
+    System.out.println(path);
+
+    try(FileOutputStream fos = new FileOutputStream(path)){
+      fos.write(data);
+      model.addAttribute("msgUpload","File uploaded successfully");
+      model.addAttribute("fileName",file.getOriginalFilename());
+    }
+    System.out.println("File uploaded successfully");
+
     return "fileSuccess";
   }
 }
--- spring/spring-mvc/src/main/webapp/templates/fileSuccess.jsp
+++ spring/spring-mvc/src/main/webapp/templates/fileSuccess.jsp
@@ -1,9 +1,12 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page isELIgnored="false" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
     <h1>File uploaded successfully</h1>
+    <img alt="altTextForFileUpload" width="1500px" height="1500px" src="<c:url value="/static/images/${fileName}" />">
 </body>
 </html>
