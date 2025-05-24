d8c03d76e29f067f188b276b40e4c2af359c05eb
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2025-05-24 15:20:28 +0530

    Added logback classic and used logs instead of Sys out

--- java/Generics/src/main/java/com/java/generics/simple/model/Main.java
+++ java/Generics/src/main/java/com/java/generics/simple/model/Main.java
@@ -1,13 +1,20 @@
 package com.java.generics.simple.model;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
 
   public static void main(String[] args) {
     Box<Integer> box = new Box<>();
     box.setValue(10);
-    System.out.println(box.getValue());
+    log.info(box.getValue().toString());
+    try{
+      box.setValue(Integer.valueOf("Hello"));
+    } catch (NumberFormatException e) {
+      log.error(e.getMessage());
+    }
 
-    box.setValue(Integer.valueOf("Hello"));
     System.out.println(box.getValue());
   }
 
