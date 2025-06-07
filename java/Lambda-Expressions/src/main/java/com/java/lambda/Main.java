058e04738bb2d7666940640cd2c5c9acbca697d6
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2025-06-07 11:42:55 +0530

    Anonymous inner class

--- java/Lambda-Expressions/src/main/java/com/java/lambda/Main.java
+++ java/Lambda-Expressions/src/main/java/com/java/lambda/Main.java
@@ -4,7 +4,21 @@ package com.java.lambda;
 public class Main {
 
   public static void main(String[] args) {
+    MyInterface myLambda = () -> System.out.println("Hello and welcome! ");
 
+    myLambda.myMethod();
+
+    MyInterfaceAdd myLambdaAdd = (int a, int b) -> a + b;
+
+    System.out.println(myLambdaAdd.myMethodAdd(1, 2));
+
+    Greeting greeting = new Greeting(){
+      public void myMethod() {
+        System.out.println("Hello World!");
+      }
+    };
+
+    greeting.myMethod();
   }
 }
 
