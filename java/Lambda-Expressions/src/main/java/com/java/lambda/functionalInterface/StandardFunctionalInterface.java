cd6fb87dd69c2a6d0d94c187647a3abe33b983c9
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2025-06-07 16:42:54 +0530

    Using predicate to send in lambda expressions as conditions

---
+++ java/Lambda-Expressions/src/main/java/com/java/lambda/functionalInterface/StandardFunctionalInterface.java
@@ -0,0 +1,41 @@
+package com.java.lambda.functionalInterface;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class StandardFunctionalInterface {
+
+  public static void main(String[] args) {
+    List<Person> personList = Arrays.asList(
+        new Person("John", "Taker"),
+        new Person("Jane", "Doe"),
+        new Person("John", "Smith"),
+        new Person("Jane", "Smith"),
+        new Person("Undertaker", "Doe"),
+        new Person("Kane", "Doe")
+    );
+
+    personList.sort((p1, p2) -> p1.getLastName().compareTo(p2.getLastName()));
+
+    System.out.println("Printing everything");
+    printConditionally(personList, p -> true);
+
+    System.out.println("Printing Only ends with");
+    printConditionally(personList, p -> p.getLastName().startsWith("D"));
+
+    System.out.println("Printing ends with and starts with");
+    printConditionally(personList,
+        p -> p.getLastName().endsWith("e") && p.getFirstName().startsWith("J"));
+
+  }
+
+  public static void printConditionally(List<Person> personList, Predicate<Person> predicate) {
+    for (Person person : personList) {
+      if (predicate.test(person)) {
+        System.out.println(person.getFirstName() + " " + person.getLastName());
+      }
+    }
+  }
+
+}
--- java/Lambda-Expressions/src/main/java/com/java/lambda/model/Person.java
+++
@@ -1,4 +0,0 @@
-package com.java.lambda.model;
-
-public class Person {
-}
