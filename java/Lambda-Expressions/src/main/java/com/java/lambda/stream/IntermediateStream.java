b0207a9a27af6b218870c315d328475d0ca6305a
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2025-07-19 17:59:03 +0530

    Getting the hang of terminal operations

---
+++ java/Lambda-Expressions/src/main/java/com/java/lambda/stream/IntermediateStream.java
@@ -0,0 +1,20 @@
+package com.java.lambda.stream;
+
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+public class IntermediateStream {
+
+  public static void main(String[] args) {
+    IntStream.iterate( 'A', i -> i <= (int) 'z', i -> i + 1)
+        //.filter(i -> Character.toUpperCase(i) > 'E')
+        .filter(Character::isLetter)
+        .dropWhile(i -> i <= (int) 'E')
+        .takeWhile(i -> i <= (int) 'Z')
+        .skip(2)
+        .boxed()
+        .sorted(Comparator.reverseOrder())
+        //.limit(10)
+        .forEach(i -> System.out.print((char) i.intValue() + " "));
+  }
+}
