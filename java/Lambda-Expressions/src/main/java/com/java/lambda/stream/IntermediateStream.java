8798d26b1862f47c1271f7a9f971f49bd1a33a7c
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2025-07-19 20:49:32 +0530

    Student engagement system setup

--- java/Lambda-Expressions/src/main/java/com/java/lambda/stream/IntermediateStream.java
+++ java/Lambda-Expressions/src/main/java/com/java/lambda/stream/IntermediateStream.java
@@ -2,11 +2,12 @@ package com.java.lambda.stream;
 
 import java.util.Comparator;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class IntermediateStream {
 
   public static void main(String[] args) {
-    IntStream.iterate( 'A', i -> i <= (int) 'z', i -> i + 1)
+    IntStream.iterate('A', i -> i <= (int) 'z', i -> i + 1)
         //.filter(i -> Character.toUpperCase(i) > 'E')
         .filter(Character::isLetter)
         .dropWhile(i -> i <= (int) 'E')
@@ -16,5 +17,19 @@ public class IntermediateStream {
         .sorted(Comparator.reverseOrder())
         //.limit(10)
         .forEach(i -> System.out.print((char) i.intValue() + " "));
+
+    Course pymc = new Course("PYMC", "Python Masterclass");
+    Course jmc = new Course("JMC", "Java Masterclass");
+    Student tim = new Student("AU", 2019, 30, "M",
+        true, jmc, pymc);
+    System.out.println(tim);
+
+    tim.watchLecture("JMC", 10, 5, 2019);
+    tim.watchLecture("PYMC", 7, 7, 2020);
+    System.out.println(tim);
+
+    Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
+        .limit(10)
+        .forEach(System.out::println);
   }
 }
