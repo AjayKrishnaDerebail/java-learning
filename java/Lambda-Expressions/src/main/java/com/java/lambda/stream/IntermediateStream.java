3925f3dda0c0c771754521daf83cebd7811c9e7f
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2025-07-19 21:13:10 +0530

    Restored files

--- java/Lambda-Expressions/src/main/java/com/java/lambda/stream/IntermediateStream.java
+++ java/Lambda-Expressions/src/main/java/com/java/lambda/stream/IntermediateStream.java
@@ -2,7 +2,6 @@ package com.java.lambda.stream;
 
 import java.util.Comparator;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class IntermediateStream {
 
@@ -18,18 +17,5 @@ public class IntermediateStream {
         //.limit(10)
         .forEach(i -> System.out.print((char) i.intValue() + " "));
 
-    Course pymc = new Course("PYMC", "Python Masterclass");
-    Course jmc = new Course("JMC", "Java Masterclass");
-    Student tim = new Student("AU", 2019, 30, "M",
-        true, jmc, pymc);
-    System.out.println(tim);
-
-    tim.watchLecture("JMC", 10, 5, 2019);
-    tim.watchLecture("PYMC", 7, 7, 2020);
-    System.out.println(tim);
-
-    Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
-        .limit(10)
-        .forEach(System.out::println);
   }
 }
