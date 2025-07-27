d77ceee25864877d4e5ac967d519acb652a72b47
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2025-07-27 16:34:03 +0530

    Using file operations

--- java/java-io-operations/src/main/java/com/io/operations/Main.java
+++ java/java-io-operations/src/main/java/com/io/operations/Main.java
@@ -1,6 +1,7 @@
 package com.io.operations;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,9 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 public class Main {
 
   public static void main(String[] args) {
-    log.info("Hello and welcome!");
-    testFile();
-    testFileUsingPath();
+    //log.info("Hello and welcome!");
+    //testFile();
+    //testFileUsingPath();
+    useFile();
   }
 
   private static void testFile() {
@@ -33,4 +35,27 @@ public class Main {
     }
     log.info("File exists using path , file path is : {}", file.getAbsolutePath());
   }
+
+  private static void useFile() {
+    File file = new File("files/test.csv");
+    boolean fileExists = false;
+    if (file.exists()) {
+      log.info("File existed deleting current file");
+      fileExists = !file.delete();
+    }
+    if (!fileExists) {
+      log.info("Creating new file");
+      try {
+        boolean created = file.createNewFile();
+        log.info("File created : {} , in path : {}", created, file.getAbsolutePath());
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    log.info("File exists , file path {}", file.getAbsolutePath());
+    if(file.canWrite()){
+      log.info("File is writable");
+    }
+  }
+
 }
