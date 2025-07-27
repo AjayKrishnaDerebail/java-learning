31567f231e3a8f0d78b11b4ec698e8c3577d7c4b
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2025-07-27 16:09:24 +0530

    Basic file operations

--- java/java-io-operations/src/main/java/com/io/operations/Main.java
+++ java/java-io-operations/src/main/java/com/io/operations/Main.java
@@ -1,5 +1,6 @@
 package com.io.operations;
 
+import java.io.File;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -7,5 +8,15 @@ public class Main {
 
   public static void main(String[] args) {
     log.info("Hello and welcome!");
+    testFile();
+  }
+
+  private static void testFile() {
+    File file = new File("files/test.csv");
+    if(!file.exists()) {
+      log.info("File does not exist");
+      return;
+    }
+    log.info("File exists");
   }
 }
