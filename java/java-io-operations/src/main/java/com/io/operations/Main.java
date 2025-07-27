1c97aa4f0bc749d289cb7e8fccbeeb5b1d788db5
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2025-07-27 16:17:41 +0530

    Using path

--- java/java-io-operations/src/main/java/com/io/operations/Main.java
+++ java/java-io-operations/src/main/java/com/io/operations/Main.java
@@ -1,6 +1,9 @@
 package com.io.operations;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -9,14 +12,25 @@ public class Main {
   public static void main(String[] args) {
     log.info("Hello and welcome!");
     testFile();
+    testFileUsingPath();
   }
 
   private static void testFile() {
     File file = new File("files/test.csv");
-    if(!file.exists()) {
+    if (!file.exists()) {
       log.info("File does not exist");
       return;
     }
-    log.info("File exists");
+    log.info("File exists , file path {}", file.getAbsolutePath());
+  }
+
+  private static void testFileUsingPath() {
+    File file = new File("files/test.csv");
+    Path path = Paths.get("files/test.csv");
+    if (!Files.exists(path)) {
+      log.info("File does not exist using path");
+      return;
+    }
+    log.info("File exists using path , file path is : {}", file.getAbsolutePath());
   }
 }
