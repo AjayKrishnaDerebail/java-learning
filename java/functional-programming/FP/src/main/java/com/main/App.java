9c3df59717b8ac1d6b7d0209ad11516af4343796
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2025-11-23 16:32:53 +0530

    Section 6 lambdas

--- java/functional-programming/FP/src/main/java/com/main/App.java
+++ java/functional-programming/FP/src/main/java/com/main/App.java
@@ -2,6 +2,7 @@ package com.main;
 
 import com.main.comparision.ImperativeVsDeclarative;
 import com.main.lambdaintros.RunnableExample;
+import java.util.Comparator;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
@@ -15,15 +16,17 @@ public class App {
 
   public static void main(String[] args) {
 
-    // Section 5
+    /* Section 5 */
 
     val declarativeStyleList = ImperativeVsDeclarative.getNonDuplicateListDeclarativeStyle();
     val imperativeStyleList = ImperativeVsDeclarative.getNonDuplicateListImperativeStyle();
 
-    log.info("{}" , declarativeStyleList);
-    log.info("{}" , imperativeStyleList);
+    log.info("{}", declarativeStyleList);
+    log.info("{}", imperativeStyleList);
 
-    //Section 6
+    /* Section 6 */
+
+    //Runnable
 
     Thread t1 = new Thread(new RunnableExample());
     t1.start();
@@ -31,6 +34,18 @@ public class App {
     final Runnable runnableLambda = () -> log.info("Inside runnable lambda");
 
     new Thread(runnableLambda).start();
+
+    //Comparator
+
+    Comparator<Integer> integerComparator = (x, y) -> x.compareTo(y); //Integer::compareTo
+
+    log.info("{}", integerComparator.compare(5, 10));
+
+    log.info("{}", integerComparator.compare(10, 5));
+
+    Comparator<Integer> integerComparatorWithNaturalOrderMethodReference = Comparator.naturalOrder();
+
+    log.info("{}", integerComparatorWithNaturalOrderMethodReference.compare(5, 5));
   }
 
 }
