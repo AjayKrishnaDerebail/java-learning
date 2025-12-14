2dfccd3bc3080240a216d244519b6ea8d772b83f
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2025-12-14 20:57:10 +0530

    Code Refactoring

--- java/functional-programming/FP/src/main/java/com/main/App.java
+++ java/functional-programming/FP/src/main/java/com/main/App.java
@@ -1,15 +1,8 @@
 package com.main;
 
-import static com.main.section.section7.PredicateExamples.p1;
-
-import com.main.database.StudentDatabase;
 import com.main.section.section5.SectionFive;
-import com.main.section.section7.ConsumerAndPredicate;
-import com.main.section.section7.ConsumerAndPredicateChallenge;
-import com.main.section.section7.ConsumerExamples;
 import com.main.section.section6.SectionSix;
-import com.main.section.section7.FunctionExample;
-import com.main.section.section7.PredicateExamples;
+import com.main.section.section7.Section7;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -27,38 +20,7 @@ public class App {
 
     /* Section 7 */
 
-    {
-
-      ConsumerExamples.consumerExamples();
-
-      log.info("Predicate examples");
-
-      PredicateExamples.filterStudentByGrade();
-
-      log.info("Predicate and Consumer examples");
-
-      ConsumerAndPredicate.printName();
-
-      log.info("Predicate and Consumer examples challenge by ChatGPT");
-
-      ConsumerAndPredicateChallenge.purePredicateChallenge();
-
-      ConsumerAndPredicateChallenge.purePredicateChallengeUsingStream();
-
-      log.info("Challenge combination epic");
-
-      ConsumerAndPredicateChallenge.ConsumerAndPredicateChatGptChallenge();
-
-      log.info("Challenge combination epic rule based solution");
-
-      ConsumerAndPredicateChallenge.ruleBasedExecution();
-
-      log.info("BiFunction example");
-
-      log.info("{}",FunctionExample.biFunction.apply(StudentDatabase.getAllStudents(), p1));
-    }
-
-
+    Section7.javaUtilFunctionPackageExamples();
   }
 
 }
--- java/functional-programming/FP/src/main/java/com/main/section/section7/Section7.java
+++ java/functional-programming/FP/src/main/java/com/main/section/section7/Section7.java
@@ -1,5 +1,33 @@
 package com.main.section.section7;
 
+import static com.main.section.section7.PredicateExamples.p1;
+
+import com.main.database.StudentDatabase;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Section7 {
+
+  public static void javaUtilFunctionPackageExamples() {
+    ConsumerExamples.consumerExamples();
+
+    log.info("Predicate examples");
+    PredicateExamples.filterStudentByGrade();
+
+    log.info("Predicate and Consumer examples");
+    ConsumerAndPredicate.printName();
+
+    log.info("Predicate and Consumer examples challenge by ChatGPT");
+    ConsumerAndPredicateChallenge.purePredicateChallenge();
+    ConsumerAndPredicateChallenge.purePredicateChallengeUsingStream();
+
+    log.info("Challenge combination epic");
+    ConsumerAndPredicateChallenge.ConsumerAndPredicateChatGptChallenge();
+    log.info("Challenge combination epic rule based solution");
+    ConsumerAndPredicateChallenge.ruleBasedExecution();
+
+    log.info("BiFunction example");
+    log.info("{}", FunctionExample.biFunction.apply(StudentDatabase.getAllStudents(), p1));
+  }
 
 }
