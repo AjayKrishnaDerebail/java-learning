5b2cf79028c2d5c4c889c63b0526f49c4479a4f3
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2026-02-01 14:30:16 +0530

    Multiline string

--- java/functional-programming/com.java_9_to_25/src/main/java/com/java_9_to_25/Main.java
+++ java/functional-programming/com.java_9_to_25/src/main/java/com/java_9_to_25/Main.java
@@ -4,4 +4,12 @@ void main() {
     final var nameMain = List.of("Dilip");
     IO.println(nameMain);
     IO.println(name + ", hello and welcome!");
+
+    final var multiLineString = """
+        This is a
+          multiline String
+        This is awesome !!!
+        """;
+
+  IO.print(multiLineString);
 }
