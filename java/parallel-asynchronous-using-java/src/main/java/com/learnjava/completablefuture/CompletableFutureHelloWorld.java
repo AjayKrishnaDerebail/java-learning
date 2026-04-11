784107f7399d9f01ace64216b4797befedb8f23f
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2026-04-11 12:54:06 +0530

    Creating helloWorld method to unit test

--- java/parallel-asynchronous-using-java/src/main/java/com/learnjava/completablefuture/CompletableFutureHelloWorld.java
+++ java/parallel-asynchronous-using-java/src/main/java/com/learnjava/completablefuture/CompletableFutureHelloWorld.java
@@ -4,11 +4,26 @@ import static com.learnjava.util.LoggerUtil.log;
 
 import com.learnjava.service.HelloWorldService;
 import java.util.concurrent.CompletableFuture;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
 
+@RequiredArgsConstructor
+@SuppressWarnings("unused")
 public class CompletableFutureHelloWorld {
 
+  private final HelloWorldService helloWorldService;
+
+  public CompletableFuture<String> helloWorld(final HelloWorldService helloWorldService){
+    //noinspection Convert2MethodRef
+    return CompletableFuture.supplyAsync(() -> helloWorldService.helloWorld())
+        .thenApply(String::toUpperCase)
+        //.thenAccept(result -> log("Result is " + result))
+        //.join()
+        ;
+  }
+
   static void main() {
-    HelloWorldService helloWorldService = new HelloWorldService();
+    val helloWorldService = new HelloWorldService();
 
     //noinspection Convert2MethodRef
     CompletableFuture.supplyAsync(() -> helloWorldService.helloWorld())
---
+++ java/parallel-asynchronous-using-java/src/test/java/com/learnjava/completablefuture/CompletableFutureHelloWorldTest.java
@@ -0,0 +1,5 @@
+package com.learnjava.completablefuture;
+
+public class CompletableFutureHelloWorldTest {
+
+}
