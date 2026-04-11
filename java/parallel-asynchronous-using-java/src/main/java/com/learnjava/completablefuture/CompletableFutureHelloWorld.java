fc289f9a2126921053639354b849956d16a8e5bc
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2026-04-11 12:40:11 +0530

    Simple example for CompletableFuture

---
+++ java/parallel-asynchronous-using-java/src/main/java/com/learnjava/completablefuture/CompletableFutureHelloWorld.java
@@ -0,0 +1,21 @@
+package com.learnjava.completablefuture;
+
+import static com.learnjava.util.LoggerUtil.log;
+
+import com.learnjava.service.HelloWorldService;
+import java.util.concurrent.CompletableFuture;
+
+public class CompletableFutureHelloWorld {
+
+  static void main() {
+    HelloWorldService helloWorldService = new HelloWorldService();
+
+    //noinspection Convert2MethodRef
+    CompletableFuture.supplyAsync(() -> helloWorldService.helloWorld())
+        .thenAccept(result -> log("Result is "+ result))
+        .join();
+
+    log("Done");
+  }
+
+}
