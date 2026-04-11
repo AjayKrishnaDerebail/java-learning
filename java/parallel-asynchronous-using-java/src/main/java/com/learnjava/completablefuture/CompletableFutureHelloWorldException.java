24e6f355c6131a16c1b11532b0304eb08d013107
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2026-04-11 19:05:43 +0530

    CF handle exception

---
+++ java/parallel-asynchronous-using-java/src/main/java/com/learnjava/completablefuture/CompletableFutureHelloWorldException.java
@@ -0,0 +1,63 @@
+package com.learnjava.completablefuture;
+
+import static com.learnjava.util.CommonUtil.startTimer;
+import static com.learnjava.util.CommonUtil.timeTaken;
+import static com.learnjava.util.LoggerUtil.log;
+
+import com.learnjava.service.HelloWorldService;
+import java.util.concurrent.CompletableFuture;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+
+@RequiredArgsConstructor
+@SuppressWarnings("unused")
+public class CompletableFutureHelloWorldException {
+
+  private final HelloWorldService helloWorldService;
+
+  @SuppressWarnings("Convert2MethodRef")
+  public String completableFutureThenCombine3Cfs() {
+
+    startTimer();
+    CompletableFuture<String> hello = CompletableFuture.supplyAsync(
+        () -> helloWorldService.hello());
+    CompletableFuture<String> world = CompletableFuture.supplyAsync(
+        () -> helloWorldService.world());
+    CompletableFuture<String> hi = CompletableFuture.supplyAsync(() -> helloWorldService.hi());
+
+    val result = hello
+        .handle((res, e) -> {
+          if (e != null) {
+            log("Exception is : " + e.getMessage());
+          }
+          return "";
+        })
+        .thenCombine(world, (h, w) -> h + w)
+        .handle((res, e) -> {
+          if (e != null) {
+            log("Exception during world is : " + e.getMessage());
+          }
+          return "";
+        })
+        .thenCombine(hi, (helloWorld, hiCf) -> helloWorld + hiCf)
+        .thenApply(String::toUpperCase)
+        .join();
+
+    timeTaken();
+
+    return result;
+  }
+
+  static void main() {
+    val helloWorldService = new HelloWorldService();
+
+    //noinspection Convert2MethodRef
+    CompletableFuture.supplyAsync(() -> helloWorldService.helloWorld())
+        .thenApply(String::toUpperCase)
+        .thenAccept(result -> log("Result is " + result))
+        .join();
+
+    log("Done");
+  }
+
+}
---
+++ java/parallel-asynchronous-using-java/src/test/java/com/learnjava/completablefuture/CompletableFutureHelloWorldExceptionTest.java
@@ -0,0 +1,57 @@
+package com.learnjava.completablefuture;
+
+import static com.learnjava.util.CommonUtil.stopWatchReset;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import com.learnjava.service.HelloWorldService;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@RequiredArgsConstructor
+@ExtendWith(MockitoExtension.class)
+public class CompletableFutureHelloWorldExceptionTest {
+
+  @Mock
+  private HelloWorldService helloWorldService;
+
+  @InjectMocks
+  CompletableFutureHelloWorldException completableFutureHelloWorldException;
+
+  @BeforeEach
+  void setUp(){
+    stopWatchReset();
+  }
+
+  @Test
+  void completableFutureThenCombine3CfsHandleExceptionUsing_CF_Handle() {
+
+    when(helloWorldService.hello()).thenThrow(new RuntimeException("Exception to test CF handle"));
+    when(helloWorldService.world()).thenCallRealMethod();
+    when(helloWorldService.hi()).thenCallRealMethod();
+
+    val result = completableFutureHelloWorldException.completableFutureThenCombine3Cfs();
+
+    assertEquals(" HI COMPLETABLEFUTURE", result);
+
+  }
+
+  @Test
+  void completableFutureThenCombine3CfsHandleExceptionUsing_CF_Handle_Hello_And_World() {
+
+    when(helloWorldService.hello()).thenThrow(new RuntimeException("Exception to test CF handle"));
+    when(helloWorldService.world()).thenThrow(new RuntimeException("Exception to test CF handle"));
+    when(helloWorldService.hi()).thenCallRealMethod();
+
+    val result = completableFutureHelloWorldException.completableFutureThenCombine3Cfs();
+
+    assertEquals(" HI COMPLETABLEFUTURE", result);
+
+  }
+}
