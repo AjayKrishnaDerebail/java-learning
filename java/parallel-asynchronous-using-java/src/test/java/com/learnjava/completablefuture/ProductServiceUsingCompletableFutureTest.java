e8dbf694b4a84bd71854aa191ded94a552c0ee1a
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2026-04-11 16:45:18 +0530

    Unit tests for inventory service

--- java/parallel-asynchronous-using-java/src/test/java/com/learnjava/completablefuture/ProductServiceUsingCompletableFutureTest.java
+++ java/parallel-asynchronous-using-java/src/test/java/com/learnjava/completablefuture/ProductServiceUsingCompletableFutureTest.java
@@ -4,6 +4,7 @@ import static com.learnjava.util.CommonUtil.stopWatchReset;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import com.learnjava.service.InventoryService;
 import com.learnjava.service.ProductInfoService;
 import com.learnjava.service.ReviewService;
 import lombok.val;
@@ -14,8 +15,9 @@ public class ProductServiceUsingCompletableFutureTest {
 
   private final ProductInfoService productInfoService = new ProductInfoService();
   private final ReviewService reviewService = new ReviewService();
+  private final InventoryService inventoryService = new InventoryService();
   ProductServiceUsingCompletableFuture productService = new ProductServiceUsingCompletableFuture(
-      productInfoService, reviewService);
+      productInfoService, reviewService, inventoryService);
 
   @BeforeEach
   void setUp() {
@@ -45,6 +47,20 @@ public class ProductServiceUsingCompletableFutureTest {
           assertEquals(productId, p.getProductInfo().getProductId());
         }
     );
+  }
+
+  @Test
+  void retrieveProductDetailsWithInventory() {
+
+    String productId = "ABC123";
+    val product = productService.retrieveProductDetailsWithInventory(productId);
+
+    assertNotNull(product);
+    assertEquals(4 , product.getProductInfo().getProductOptions().size());
+    product.getProductInfo().getProductOptions()
+        .forEach(productOption ->
+            assertNotNull(productOption.getInventory()));
+
   }
 
 }
