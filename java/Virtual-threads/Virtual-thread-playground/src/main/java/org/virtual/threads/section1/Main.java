97542ab6cc770e50ac68254e5f2b49fe71981b64
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2026-04-16 09:11:30 +0530

    Platform threads with countdown latch

--- java/Virtual-threads/Virtual-thread-playground/src/main/java/org/virtual/threads/section1/Main.java
+++ java/Virtual-threads/Virtual-thread-playground/src/main/java/org/virtual/threads/section1/Main.java
@@ -2,11 +2,14 @@ package org.virtual.threads.section1;
 
 import java.util.concurrent.CountDownLatch;
 
+@SuppressWarnings("unused")
 public class Main {
   private static final int MAX_PLATFORM_THREADS = 10;
+  private static final int MAX_VIRTUAL_THREADS = 100;
 
   static void main() throws InterruptedException {
-    platformBuilderAndDaemonThreadsWithCountdownLatch();
+    //platformBuilderAndDaemonThreadsWithCountdownLatch();
+    virtualThreadsWithCountDownLatch();
   }
 
   public static void platformBuilderAndDaemonThreadsWithCountdownLatch()
@@ -15,6 +18,25 @@ public class Main {
     var latch = new CountDownLatch(MAX_PLATFORM_THREADS);
     var builder = Thread.ofPlatform().daemon().name("daemon",10);
     for(var i=0; i< MAX_PLATFORM_THREADS; i++) {
+      int j = i ;
+      Thread thread = builder.unstarted(() -> {
+        Task.ioIntensiveTask(j);
+        latch.countDown();
+      });
+      thread.start();
+    }
+    latch.await();
+  }
+
+  /**
+   * Virtual threads are daemon threads by default
+   */
+  public static void virtualThreadsWithCountDownLatch()
+      throws InterruptedException {
+
+    var latch = new CountDownLatch(MAX_VIRTUAL_THREADS);
+    var builder = Thread.ofVirtual().name("virtual-",1);
+    for(var i=0; i< MAX_VIRTUAL_THREADS; i++) {
       int j = i ;
       Thread thread = builder.unstarted(() -> {
         Task.ioIntensiveTask(j);
