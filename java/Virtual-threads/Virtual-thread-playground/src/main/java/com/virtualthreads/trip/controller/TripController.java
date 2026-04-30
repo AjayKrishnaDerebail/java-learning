c124377b51bc698efc748e9ce8c68a3bf53cec7b
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2026-04-30 09:47:22 +0530

    Added logs

--- java/Virtual-threads/Virtual-thread-playground/src/main/java/com/virtualthreads/trip/controller/TripController.java
+++ java/Virtual-threads/Virtual-thread-playground/src/main/java/com/virtualthreads/trip/controller/TripController.java
@@ -6,6 +6,7 @@ import com.virtualthreads.trip.dto.TripReservationRequest;
 import com.virtualthreads.trip.service.TripPlanService;
 import com.virtualthreads.trip.service.TripReservationService;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,14 +30,20 @@ public class TripController {
 
   @GetMapping("{airportCode}")
   public TripPlan planTrip(@PathVariable("airportCode") String airportCode) {
+    val startTime = System.currentTimeMillis();
     log.info("Planning trip for airport code: {} and thread name : {} and isVirtual : {}", airportCode,
         Thread.currentThread().getName(),Thread.currentThread().isVirtual());
-    return this.planService.getTripPlan(airportCode);
+    var planResponse = planService.getTripPlan(airportCode);
+    log.info("Planning service took {} ms", System.currentTimeMillis() - startTime);
+    return planResponse;
   }
 
   @PostMapping("reserve")
   public FlightReservationResponse reserveFlight(@RequestBody TripReservationRequest request) {
-    return this.reservationService.reserve(request);
+    val startTime = System.currentTimeMillis();
+    val reservationResponse = this.reservationService.reserve(request);
+    log.info("Reservation service took {} ms", System.currentTimeMillis() - startTime);
+    return reservationResponse;
   }
 
 }
