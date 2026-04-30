ab7c40e59efe31b273ba29721c26f733b9558dd7
Ajay Krishna Derebail <ajaykrishnaderebail@gmail.com>
2026-04-30 09:28:19 +0530

    Added trip controller

---
+++ java/Virtual-threads/Virtual-thread-playground/src/main/java/com/virtualthreads/trip/controller/TripController.java
@@ -0,0 +1,38 @@
+package com.virtualthreads.trip.controller;
+
+import com.virtualthreads.trip.dto.FlightReservationResponse;
+import com.virtualthreads.trip.dto.TripPlan;
+import com.virtualthreads.trip.dto.TripReservationRequest;
+import com.virtualthreads.trip.service.TripPlanService;
+import com.virtualthreads.trip.service.TripReservationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("trip")
+public class TripController {
+
+  private final TripPlanService planService;
+  private final TripReservationService reservationService;
+
+  public TripController(TripPlanService planService,
+      TripReservationService reservationService) {
+    this.planService = planService;
+    this.reservationService = reservationService;
+  }
+
+  @GetMapping("{airportCode}")
+  public TripPlan planTrip(@PathVariable String airportCode) {
+    return this.planService.getTripPlan(airportCode);
+  }
+
+  @PostMapping("reserve")
+  public FlightReservationResponse reserveFlight(@RequestBody TripReservationRequest request) {
+    return this.reservationService.reserve(request);
+  }
+
+}
